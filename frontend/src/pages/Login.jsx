b2054e185b1beccd22dfa3e723bacf13bf816d49
Eslam <eslam.hamada2026@gmail.com>
2026-08-30 01:54:34 +0300

    Add: Login & Register Frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Login.css';
+import '../style/Login.css';
 
 function Login() {
     const navigate = useNavigate();
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "./Register.css";
+import "../style/Register.css";
 
 function Register() {
     const navigate = useNavigate();
